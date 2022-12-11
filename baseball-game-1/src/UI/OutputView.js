b8610188f57399de8d585add12bec6c10a8212bf
doheeMac <thsehgml37@korea.ac.kr>
2022-12-11 22:36:37 +0900

    docs: 기능 목록 업데이트

--- baseball-game-1/src/UI/OutputView.js
+++ baseball-game-1/src/UI/OutputView.js
@@ -5,5 +5,22 @@ const OutputView = {
 	printWelcome() {
 		Console.print(MESSAGE['WELCOME']);
 	},
+
+	printScore(gameReult) {
+		const FORMATTED_SCORE = s;
+	},
+
+	scorePrintFormatter(gameResult) {
+		if (gameReult['STRIKE'] === 3) {
+			return MESSAGE['SUCCESS'];
+		}
+		if (gameReult['BALL'] === 0 && gameReult['STRIKE'] === 0) {
+			return MESSAGE['LOST'];
+		}
+		let ballStrike = '';
+		if (gameReult['BALL'] > 0) {
+			ballStrike += gameResult['BALL'] + MESSAGE['BALL'];
+		}
+	},
 };
 module.exports = OutputView;
